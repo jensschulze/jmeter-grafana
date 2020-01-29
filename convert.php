d67549f00899e2fe1bb6355dc8cc7bfff5948216
Jens Schulze <jdschulze@brainbits.net>
2020-01-29 14:50:13 +0100

    Dockerize

--- convert.php
+++ convert.php
@@ -27,8 +27,17 @@ class JmeterData
     public const CONNECT = 16;
 }
 
+$chunkSize = (int) getenv('CONVERT_SEND_CHUNK_SIZE');
+$influxdbHost = getenv('CONVERT_INFLUXDB_HOST');
+$influxdbPort = (int) getenv('CONVERT_INFLUXDB_PORT');
+$influxdbDb = getenv('CONVERT_INFLUXDB_DB');
+$influxdbUser = getenv('CONVERT_INFLUXDB_USER');
+$influxdbPassword = getenv('CONVERT_INFLUXDB_PASSWORD');
+
 ini_set('memory_limit', '-1');
 include 'vendor/autoload.php';
+
+echo 'Reading data from var/data.csv'.PHP_EOL;
 
 // Read CSV into $csv array
 $handle = fopen('var/data.csv', 'rb');
@@ -44,18 +53,46 @@ while (true) {
 
 fclose($handle);
 
+echo 'Writing data to InfluxDB.'.PHP_EOL;
+
 // Remove column titles from $csv
 array_shift($csv);
 
 // Write "latency" and "elapsed" into the respective time series
-$client = new Client('127.0.0.1', 8086, 'jmeter', 'jmeter');
-$database = $client->selectDB('jmeter');
+$client = new Client($influxdbHost, $influxdbPort, $influxdbUser, $influxdbPassword);
+$database = $client->selectDB($influxdbDb);
 
-$points = [];
-foreach ($csv as $row) {
-    $points[] = new Point('latency', (int) $row[JmeterData::LATENCY] , ['label' => $row[JmeterData::LABEL], 'statuscode' => (int) $row[JmeterData::RESPONSECODE]], [], $row[JmeterData::TIMESTAMP]);
-    $points[] = new Point('elapsed', (int) $row[JmeterData::ELAPSED] , ['label' => $row[JmeterData::LABEL], 'statuscode' => (int) $row[JmeterData::RESPONSECODE]], [], $row[JmeterData::TIMESTAMP]);
+foreach (array_chunk($csv, $chunkSize) as $chunk) {
+    $points = [];
+    foreach ($chunk as $row) {
+        try {
+            $points[] = new Point(
+                'latency',
+                (int) $row[JmeterData::LATENCY],
+                ['label' => $row[JmeterData::LABEL], 'statuscode' => (int) $row[JmeterData::RESPONSECODE]],
+                [],
+                $row[JmeterData::TIMESTAMP]
+            );
+            $points[] = new Point(
+                'elapsed',
+                (int) $row[JmeterData::ELAPSED],
+                ['label' => $row[JmeterData::LABEL], 'statuscode' => (int) $row[JmeterData::RESPONSECODE]],
+                [],
+                $row[JmeterData::TIMESTAMP]
+            );
+        } catch (Throwable $e) {
+            echo 'Error'.PHP_EOL;
+            echo sprintf('memory: %d', memory_get_peak_usage(true) / (1024 * 1024)).PHP_EOL;
+            echo sprintf(
+                    'timestamp: %d; label: "%s"; statuscode: "%s"',
+                    $row[JmeterData::TIMESTAMP],
+                    $row[JmeterData::LABEL],
+                    $row[JmeterData::RESPONSECODE]
+                ).PHP_EOL;
+            echo (string) $e.PHP_EOL;
+        }
+    }
+
+    $database->writePoints($points, Database::PRECISION_MILLISECONDS);
+    echo sprintf('memory: %d', memory_get_peak_usage(true) / (1024 * 1024)).PHP_EOL;
 }
-
-$database->writePoints($points, Database::PRECISION_MILLISECONDS);
-
